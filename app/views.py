5db2b55bdc0597ba6b56b25b7aa450980a1d42e2
LucasHollas <33184880+LucasHollas@users.noreply.github.com>
2021-10-28 17:18:05 -0300

    Add files via upload

--- app/views.py
+++ app/views.py
@@ -1,13 +1,23 @@
+from django import db
+from django.core import paginator
 from django.shortcuts import redirect, render
 from app.forms import CargosForm
 from app.models import cargos
+from django.core.paginator import Paginator
 
 
 def home(request):
     data = {}
-    data["db"] = cargos.objects.all()
+    search = request.GET.get('search')
+    if search:
+        data['db'] = cargos.objects.filter(cargo__icontains=search)
+    else:
+        data['db'] = cargos.objects.all()
+    all = cargos.objects.all()
+    paginator = Paginator(all, 8)
+    pages = request.GET.get('page')
+    data["db"] = paginator.get_page(pages)
     return render(request, "index.html", data)
-
 
 def form(request):
     data = {}
@@ -38,4 +48,9 @@ def update(request, pk):
     form = CargosForm(request.POST or None, instance=data['db'])
     if form.is_valid():
        form.save() 
+    return redirect('home')
+
+def delete(request, pk):
+    db = cargos.objects.get(pk = pk)
+    db.delete()
     return redirect('home')
